b1c704bdf48a81022814253481dbb02f0a6919e7
Milen Mihaylov <milen.m.mihailov@gmail.com>
2021-11-20 01:40:00 +0200

    Exersices for Classes and Objects

---
+++ 02.ClassesAndObjects/Lab/demo.py
@@ -0,0 +1,28 @@
+class Laptop:
+    brand = "Dell"
+
+    def __init__(self, name):
+        self.name = name
+
+    # def __str__(self):
+    #     return f'This is brand {self.brand} laptop'
+
+    def __repr__(self):
+        return f'This is brand {self.brand} laptop2222'
+
+
+l1 = Laptop('Latitude 5000')
+l2 = Laptop('Latitude 5000')
+l3 = l1
+print(l1.name)
+print(l3.name)
+l1.name = 'new name'
+# Laptop.brand = 'HP'
+
+print(l1 == l2)
+print(l1.name == l2.name)
+print(l2.brand)
+print(l3 == l1)
+print(l1.name)
+print(l3.name)
+print(l1)
